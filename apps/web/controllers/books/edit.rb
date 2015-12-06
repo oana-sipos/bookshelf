13a0d23b65a4ca6dd96a3639233445681338e34b
Oana Sipos <oana.sipos@up-nxt.com>
2015-12-06 22:14:02 +0100

    Implement update action.

--- apps/web/controllers/books/edit.rb
+++ apps/web/controllers/books/edit.rb
@@ -2,7 +2,10 @@ module Web::Controllers::Books
   class Edit
     include Web::Action
 
+    expose :book
+
     def call(params)
+      @book = BookRepository.find(params[:id])
     end
   end
 end
--- apps/web/controllers/books/update.rb
+++ apps/web/controllers/books/update.rb
@@ -2,10 +2,23 @@ module Web::Controllers::Books
   class Update
     include Web::Action
 
+    expose :book
+
+    params do
+      param :id, presence: true
+      param :book do
+        param :title,  presence: true
+        param :author, presence: true
+      end
+    end
+
     def call(params)
-      book = BookRepository.find(params[:id])
-      BookRepository.update(book)
-      redirect_to routes.books_path
+      @book = BookRepository.find(params[:id])
+      if params.valid?
+        @book.update(params[:book])   # update of the entity
+        BookRepository.update(@book)  # update of the database
+        redirect_to routes.books_path
+      end
     end
   end
 end
